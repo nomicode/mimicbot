821b03b3ee2634db7cdacb7823af3acb80edaff9
Naomi Slater <nslater@tumbolia.org>
2017-09-25 04:53:27 +0200

    add twitter functionality

--- src/mimicbot/cli.py
+++ src/mimicbot/cli.py
@@ -1,7 +1,18 @@
-import mimicbot
+import sys
 
-def main():
+import mimicbot
+from mimicbot import twitter
+
+def main(*args):
     bot = mimicbot.Bot()
     text = bot.get_text()
     print("finished\n")
     print(text)
+    try:
+        name = sys.argv[1]
+        print(name)
+        client = twitter.Client(name)
+        client.post(text)
+    except IndexError:
+        pass
+
