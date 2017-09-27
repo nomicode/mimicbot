d8d8fbb971b14564a93125f39ea1d470f684d8f5
Naomi Slater <nslater@tumbolia.org>
2017-09-27 21:24:11 +0200

    add a release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -ex
+
+if git status -s > /dev/null; then
+    echo "unsaved changes"
+    # exit 1
+fi
+
+git fetch origin > /dev/null
+
+version=`python setup.py --version`
+
+if git tag | grep $version > /dev/null; then
+    echo "tag exists"
+    exit 1
+fi
+
+# build and upload
+rm -f dist/*
+python setup.py sdist bdist_wheel --universal
+twine upload dist/*
+
+# tag release
+git tag -a "$version" -m "tagging $version"
+git push --tags
